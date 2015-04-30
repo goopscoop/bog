f198382167de0a96b84036df7843079f33ee4a1e
goopscoop <codybarrus@gmail.com>
2015-04-29 21:32:36 -0700

    added tags

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -6,24 +6,37 @@ class CreaturesController < ApplicationController
 
   def new
     @creature = Creature.new
+    @tags = Tag.all
   end
 
   def create
     @creature = Creature.create(creature_params)
+    params[:creature][:tag_ids].each do |t|
+      @creature.tags << Tag.find_by_id(t) unless t.blank?
+    end
     redirect_to @creature
   end
 
   def show
     @creature = Creature.find(params[:id])
+    @tags = Tag.joins(:creatures).where(creatures: {id: params[:id]})
   end
 
   def update
-    @creature = Creature.find_by_id(params[:id]).update(creature_params)
+    @creature = Creature.find_by_id(params[:id])
+    @creature.update(creature_params)
+    params[:creature][:tag_ids].each do |t|
+      @creature.tags << Tag.find_by_id(t) unless t.blank?
+    end
+    # @creature.tags.clear
+
+    # render :json => params
     redirect_to creature_path(params[:id])
   end
 
   def edit
     @creature = Creature.find_by_id(params[:id])
+    @tags = Tag.all
   end
 
   def destroy
---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,49 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+    redirect_to @tag
+  end
+
+  def show
+    @tag = Tag.find_by_id(params[:id])
+    @creatures = Creature.joins(:tags).where(tags: { id: params[:id]})
+    # render :json => @creatures
+  end
+
+  def edit
+    @tag = Tag.find_by_id(params[:id])
+  end
+
+  def update
+    @tag = Tag.find_by_id(params[:id])
+    @tag.update(tag_params)
+    redirect_to tag_path(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find_by_id(params[:id])
+    @creatures = Creature.joins(:tags).where(tags: {id: params[:id]})
+    if @creatures.length > 0
+      flash[:danger] = "Cannot delete. Tag is associated with one or more creatures"
+      redirect_to tag_path
+    else
+      @tag.destroy
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
+end
